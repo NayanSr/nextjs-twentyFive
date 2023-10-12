5208b5debfb043967cd47f3774ea533656a2d2f5
NayanSr <nayankbps@gmail.com>
2023-10-12 17:05:58 +0600

    google-authentication_03

---
+++ note.js
@@ -0,0 +1,17 @@
+/*Video- 01
+  //! Basic setup for authentication using nextAuth 
+    next-auth.js.org> getting start
+
+ */
+
+/*Video- 02
+  //! Github authentication using nextAuth
+    finding signin user using useSession react hook  
+    conditional rendering signout and signin button
+    redirect to home/any route after signin 
+*/
+
+/*Video- 03
+  //! google signin 
+
+*/
