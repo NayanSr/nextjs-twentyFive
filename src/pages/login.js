cc8076b06d5e969aa95bfddf963d3cf627339887
NayanSr <nayankbps@gmail.com>
2023-10-12 22:17:41 +0600

    use-react-hookform-for-signin-form

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,9 +1,13 @@
+import { useForm } from "react-hook-form";
 import { Button } from "antd";
 import { GoogleOutlined, GithubOutlined } from "@ant-design/icons";
 import Head from "next/head";
 import styles from "@/styles/Login.module.css";
 import { signIn } from "next-auth/react";
 const LoginPage = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => console.log(data);
   return (
     <div>
       <Head>
@@ -24,12 +28,15 @@ const LoginPage = () => {
           />
         </div>
         <hr />
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <label htmlFor="">Your Email</label>
-          <input type="email" />
+          <input {...register("email", { required: true })} type="email" />
           <label htmlFor="">Your Password</label>
-          <input type="password" />
-          <Button>Login</Button>
+          <input
+            {...register("password", { required: true })}
+            type="password"
+          />
+          <button type="submit">Login</button>
         </form>
       </div>
     </div>
